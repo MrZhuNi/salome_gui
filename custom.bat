90b080bb826e3c57d4c9bcd8ee235572926d58e5
sln <sln@opencascade.com>
2005-10-31 14:11:57 +0000

    Custom file for Linux compilation

---
+++ custom.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set SUITROOT=W:\Projects\CATHARE_HEAD\GUI_SRC
+
+set QTDIR=D:\DevTools\qt333
+set QWTHOME=D:\DevTools\QWT
+
+set QTLIB=%QTDIR%\lib
+set QTLIBD=%QTDIR%\lib
+set QWTLIB=%QWTHOME%\lib
+set QWTLIBD=%QWTHOME%\lib
+set QWTINC=%QWTHOME%\include
+
+set PRODUCTSROOT=w:\SAL
+
+set PYTHONHOME=%PRODUCTSROOT%\Python24
+set PYTHONROOT=%PYTHONHOME%
+set PYTHONINC=%PYTHONROOT%/include
+set PYTHONLIB=%PYTHONROOT%/libs
+
+set VTKROOT=%PRODUCTSROOT%\VTK42
+set VTKINC=%VTKROOT%\include
+set VTKLIB=%VTKROOT%\lib
+set VTKBIN=%VTKROOT%\bin
+
+set CASROOT=W:\OCC\ros
+set OCCINC=%CASROOT%\inc
+set OCCLIB=%CASROOT%\lib
+set OCCLIBD=%CASROOT%\libd
+set OCCBIN=%CASROOT%\lib
+set OCCBIND=%CASROOT%\libd
+
+rem set VTKHOME=W:\VTK42
+
+set CSF_GraphicShr=%OCCLIBD%\TKOpenGl.dll
+
+set CSF_UnitsLexicon=%CASROOT%\src\UnitsAPI\Lexi_Expr.dat
+set CSF_UnitsDefinition=%CASROOT%\src\UnitsAPI\\Units.dat
+
+
