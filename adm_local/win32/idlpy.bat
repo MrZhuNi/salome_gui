a42a2c2ab9ffdd1997bc3398a5b1312f8522c0c6
mpv <mpv@opencascade.com>
2006-07-07 11:34:57 +0000

    Windows porting

--- adm_local/win32/idlpy.bat
+++
@@ -1,2 +0,0 @@
-@set PYTHONPATH=%OMNILIB%\..\python;%PYTHONPATH%
-@%OMNIROOT%\bin\x86_win32\omniidl.exe -p%OMNIORBPYROOT%\lib\python -bpython -I../../idl -I%KERNEL_ROOT_DIR%/idl/ -C../../win32/python %1
