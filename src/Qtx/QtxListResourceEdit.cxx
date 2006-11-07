76e0aac296b4dddcbfe3a257184eda996f9d8d55
ptv <ptv@opencascade.com>
2006-11-07 13:07:41 +0000

    remove uninitialised variable

--- src/Qtx/QtxListResourceEdit.cxx
+++ src/Qtx/QtxListResourceEdit.cxx
@@ -1174,7 +1174,9 @@ void QtxListResourceEdit::ColorItem::retrieve()
 */
 QtxListResourceEdit::FontItem::FontItem( const QString& title, QtxResourceEdit* edit,
                                          Item* pItem, QWidget* parent )
-: PrefItem( Font, edit, pItem, parent )
+: PrefItem( Font, edit, pItem, parent ),
+myIsSystem( true ),
+myFlags( ( int )All )
 {
   new QLabel( title, this );
   myFamilies = new QtxComboBox( false, this );
