825cc5e45152c4171578e400a01af57db648c651
ouv <ouv@opencascade.com>
2009-03-18 16:27:29 +0000

    Minor change

--- src/GLViewer/GLViewer_Context.cxx
+++ src/GLViewer/GLViewer_Context.cxx
@@ -686,6 +686,10 @@ void GLViewer_Context::deleteObject( GLViewer_Object* theObject, bool updateView
     if( myLastPicked == theObject )
         myLastPicked = 0;
 
+    theObject->unhighlight();
+    theObject->unselect();
+    theObject->setVisible( false );
+
     if ( updateViewer )
       myGLViewer2d->updateAll();
 }
