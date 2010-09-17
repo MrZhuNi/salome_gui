c871a0d800a97d0b2fab6af02f7c8d167ec19c2d
rnv <rnv@opencascade.com>
2010-09-17 05:58:36 +0000

    Fix for the bug 21947 WinTC5.1.4: Wrong error management of "Salome NoteBook".

--- src/PyConsole/PyConsole_Console.h
+++ src/PyConsole/PyConsole_Console.h
@@ -62,6 +62,10 @@ public:
   bool                isSync() const;
   void                setIsSync( const bool );
 
+  bool                isSuppressOutput() const;
+  void                setIsSuppressOutput( const bool );
+
+
   void                exec( const QString& );
   void                execAndWait( const QString& );
 
