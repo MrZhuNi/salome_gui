c16fe3288439431fad52de9696575385b8915fd4
sln <sln@opencascade.com>
2011-02-21 17:10:26 +0000

    *** empty log message ***

---
+++ src/SUIT/resources/SUIT_msg_fr.ts
@@ -0,0 +1,135 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+<context>
+    <name>@default</name>
+    <message>
+        <source>MEN_DESK_WINDOW</source>
+        <translation type="unfinished">&amp;Fenêtre</translation>
+    </message>
+    <message>
+        <source>ERR_CANT_DUMP_VIEW</source>
+        <translation type="unfinished">Impossible de clicher le contenu de la vue au fichier.</translation>
+    </message>
+    <message>
+        <source>TLT_IMAGE_FILES</source>
+        <translation type="unfinished">Fichiers Images  (*.bmp *.png *.jpg *.jpeg)</translation>
+    </message>
+    <message>
+        <source>MEN_DESK_WINDOW_CASCADE</source>
+        <translation>&amp;Cascade</translation>
+    </message>
+    <message>
+        <source>PRP_DESK_WINDOW_CASCADE</source>
+        <translation type="unfinished">Superposer les fenêtres</translation>
+    </message>
+    <message>
+        <source>ERR_DIR_NOT_EXIST</source>
+        <translation type="unfinished">Le répertoire &quot;%1&quot; n&apos;existe pas!</translation>
+    </message>
+    <message>
+        <source>ERR_FILE_NOT_DIR</source>
+        <translation type="unfinished">&quot;%1&quot; n&apos;est pas un répertoire!</translation>
+    </message>
+    <message>
+        <source>CONTINUE</source>
+        <translation type="unfinished">Continuer</translation>
+    </message>
+    <message>
+        <source>CANCEL</source>
+        <translation type="unfinished">Annuler</translation>
+    </message>
+    <message>
+        <source>ERR_FILE_NOT_EXIST</source>
+        <translation type="unfinished">Le fichier &quot;%1&quot; n&apos;existe pas!</translation>
+    </message>
+    <message>
+        <source>QUE_DOC_FILEEXISTS</source>
+        <translation type="unfinished">Le fichier %1 déjà existe. Faut-il le réécrire?</translation>
+    </message>
+    <message>
+        <source>PRP_DESK_WINDOW_ACTIVATE</source>
+        <translation type="unfinished">Activer la fenêtre</translation>
+    </message>
+    <message>
+        <source>ERR_PERMISSION_DENIED</source>
+        <translation type="unfinished">Impossible de sauvegarder le fichier &quot;%1&quot;.
+Autorisation interdite.</translation>
+    </message>
+    <message>
+               <source>ERR_ERROR</source>
+        <translation type="unfinished">Erreur</translation>
+    </message>
+    <message>
+        <source>QUE_FILE_EXISTS</source>
+        <translation type="unfinished">Le fichier &quot;%1&quot; déjà existe.
+Faut-il le réécrire?</translation>
+    </message>
+    <message>
+        <source>WRN_WARNING</source>
+        <translation type="unfinished">Avertissement</translation>
+    </message>
+    <message>
+        <source>TLT_DUMP_VIEW</source>
+        <translation type="unfinished">Clicher la Vue au Fichier</translation>
+    </message>
+    <message>
+        <source>PRP_DESK_WINDOW_TILE</source>
+        <translation type="unfinished">Placer les fenêtres sans superposition</translation>
+    </message>
+    <message>
+        <source>MEN_DESK_WINDOW_VTILE</source>
+        <translation type="unfinished">Paver &amp;Verticalement</translation>
+    </message>
+    <message>
+        <source>INF_DIRECTORIES_FILTER</source>
+        <translation type="unfinished">Répertoires</translation>
+    </message>
+    <message>
+        <source>PRP_DESK_WINDOW_VTILE</source>
+        <translation type="unfinished">Placer les fenêtres verticalement et sans superposition</translation>
+    </message>
+    <message>
+        <source>MEN_DESK_WINDOW_TILE</source>
+        <translation type="unfinished">&amp;Paver</translation>
+    </message>
+    <message>
+        <source>NAME_COLUMN</source>
+        <translation type="unfinished">Nom</translation>
+    </message>
+</context>
+<context>
+    <name>SUIT_Study</name>
+    <message>
+        <source>OPERATION_LAUNCH</source>
+        <translation type="unfinished">Lancer l&apos;Opération</translation>
+    </message>
+    <message>
+        <source>PREVIOUS_NOT_FINISHED</source>
+        <translation type="unfinished">L&apos;opération précédente n&apos;est pas aboutie et sera interrompue</translation>
+    </message>
+</context>
+<context>
+    <name>SUIT_FileDlg</name>
+    <message>
+        <source>LAB_QUICK_PATH</source>
+        <translation type="unfinished">Chemin Court:</translation>
+    </message>
+    <message>
+        <source>BUT_ADD_PATH</source>
+        <translation type="unfinished">Ajouter un chemin</translation>
+    </message>
+    <message>
+        <source>INF_DESK_DOC_OPEN</source>
+        <translation type="unfinished">Ouvrir un Fichier</translation>
+    </message>
+    <message>
+        <source>INF_DESK_DOC_SAVE</source>
+        <translation type="unfinished">Sauvegarder un Fichier</translation>
+    </message>
+    <message>
+        <source>ALL_FILES_FILTER</source>
+        <translation type="unfinished">Tous les Fichiers (*)</translation>
+    </message>
+</context>
+</TS>
