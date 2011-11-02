a24b3a0a15f15c0ced8bb0b585820e62b07d465c
stv <stv@opencascade.com>
2011-11-02 12:07:44 +0000

    Field myViewManager intialization.

--- src/SUIT/SUIT_ViewWindow.cxx
+++ src/SUIT/SUIT_ViewWindow.cxx
@@ -46,7 +46,8 @@ const int DUMP_EVENT = QEvent::User + 123;
 
 /*! Constructor.*/
 SUIT_ViewWindow::SUIT_ViewWindow( SUIT_Desktop* theDesktop )
-: QMainWindow( theDesktop )
+: QMainWindow( theDesktop ),
+myManager( 0 )
 {
   myDesktop = theDesktop;
 
