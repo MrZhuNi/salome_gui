c357169a38830c1e42049aa73a0c991ec27a115b
sln <sln@opencascade.com>
2012-07-02 07:37:28 +0000

    if SALOME_BACKUP_FOLDER is empty backup functionality is switched-off

--- src/SUIT/SUIT_Session.cxx
+++ src/SUIT/SUIT_Session.cxx
@@ -527,6 +527,10 @@ void SUIT_Session::createBackupTimer()
   if ( !app )
     return;
 
+  QString backupFolder = getBackupFolder();
+  if ( backupFolder.isEmpty() )
+    return;
+
   myBTimer = new QTimer( this );
   connect( myBTimer, SIGNAL( timeout() ), this, SLOT( onBTimer() ) );
 
@@ -534,7 +538,7 @@ void SUIT_Session::createBackupTimer()
   if ( mSec > 0  )
     myBTimer->start( mSec );
 
-  QString pref = QDir::convertSeparators( getBackupFolder() + "/" + getBackupPrefix() );
+  QString pref = QDir::convertSeparators( backupFolder + "/" + getBackupPrefix() );
   myBFolder = pref;
   int i = 0;
   do
