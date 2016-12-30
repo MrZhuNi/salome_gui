ff5cc1c0fec7d3c11f32486f62f8e51abc21fec3
Anthony Geay <anthony.geay@edf.fr>
2016-12-30 08:37:06 +0100

    Let the implementation of singleton as before

--- src/LightApp/LightApp_Application.cxx
+++ src/LightApp/LightApp_Application.cxx
@@ -5145,18 +5145,17 @@ bool LightApp_Application::checkExistingDoc()
 
 PyConsole_Interp* LightApp_Application::getPyInterp()
 {
-  static PyInterp_Auto<PyConsole_Interp> myInterp;
-  if ( myInterp.isNull() ) {
+  static PyConsole_Interp* myInterp = 0;
+  if ( !myInterp ) {
     myInterp = createPyInterp();
     myInterp->initialize();
-    myInterp->incrRef();// In Light application the PyConsole_Interp cannot been destroy. Why ?
   }
   return myInterp;
 }
 
 PyConsole_Interp* LightApp_Application::createPyInterp()
 {
-  return new PyConsole_Interp;
+  return new PyConsole_Interp();
 }
 
 #endif // DISABLE_PYCONSOLE
